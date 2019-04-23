b777bbae26f27ca0908725bc39ab3ab92d46b8f9
Erik Ylipää <erik.ylipaa@gmail.com>
2019-04-23 18:24:14 +0200

    Some silly tests

--- lorenz/tests/test_dataset.py
+++ lorenz/tests/test_dataset.py
@@ -1,18 +1,26 @@
 import unittest
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
 
-from vindel.lorenz import make_dataset, plot_3d
-import vindel.dataset
+from lorenz.lorenz import make_dataset, plot_3d
+import lorenz.dataset
 
 class TestDataset(unittest.TestCase):
     def setUp(self):
         self.path = os.path.join(os.path.split(os.path.split(os.path.dirname(__file__))[0])[0], 'data', 'lorenz.h5')
 
-
-    def test_random_iterator(self):
+    def test_random_iterator_1d(self):
         rng = np.random.RandomState(1729)
-        dataset = vindel.dataset.Dataset(self.path)
+        dataset = lorenz.dataset.Dataset(self.path, view='1d')
+        for b in dataset.random_iterator(4, 100):
+            plt.plot(np.linspace(0,1,b.shape[1]), b[:,:,0].T)
+            plt.show()
+            #plot_3d(b)
+
+    def test_random_iterator_3d(self):
+        rng = np.random.RandomState(1729)
+        dataset = lorenz.dataset.Dataset(self.path, view='3d')
         for b in dataset.random_iterator(4, 100):
             plot_3d(b)
